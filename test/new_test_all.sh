063872eca739dc4e6f0ad8cbe2be9f4b89e77a85
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-02-04 12:55:08 -0600

    added more assertions to new_test_all.sh

--- test/new_test_all.sh
+++ test/new_test_all.sh
@@ -3,3 +3,4 @@
 ./plassert.py app-2.plcore 'has_value(( Foo . result ), 20)'
 ./plassert.py app-3.plcore 'has_value(( Foo . r2 ), 5)'
 ./plassert.py let-1.plcore 'has_value(( Foo . result ), 4)'
+./plassert.py let-2.plcore 'has_value(( Foo . result ), 6) '
