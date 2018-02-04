8095f3f8759382ba4bdb78cf00463cfe028e5248
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-02-04 12:45:53 -0600

    print report after running test case

--- test/plassert.py
+++ test/plassert.py
@@ -3,9 +3,10 @@
 import sys
 import re
 import xml.etree.ElementTree as ET
+import subprocess            as SP
 
-def get_value(key):
-    root  = ET.parse(sys.argv[1]).getroot()
+def get_value(key, result):
+    root  = ET.fromstring(result)
     genv  = root.findall("genv")
     reg   = re.compile(re.escape(key) + " \|->\ ([^\ ]*)")
     result = reg.search(genv[0].text).group(1)
@@ -16,12 +17,18 @@ def get_value(key):
         return result
 
 def main():
+    result = SP.check_output(["./run-pretty.sh", sys.argv[1]])
+
+    fname = sys.argv[1]
     expr = sys.argv[2]
+
     re_has_value = re.compile("has\_value\((.*), (.*)\)")
     if re_has_value.match(expr):
         key = re_has_value.search(expr).group(1)
         val = re_has_value.search(expr).group(2)
-        print(get_value(key) == val)
+        if get_value(key, result) == val:
+            tmp = "{} has value {}, after running {}."
+            print(tmp.format(key, val, fname))
     else:
         print("Unrecognized expression")
 
