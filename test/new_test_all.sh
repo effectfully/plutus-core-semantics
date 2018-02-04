678ec41074c433e70fdef75e51e84ed74622409c
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-02-04 12:53:07 -0600

    update new_test_all.sh

--- test/new_test_all.sh
+++ test/new_test_all.sh
@@ -1,6 +1,5 @@
 # Will eventually replace `test_all.sh`.
-./run-pretty.sh app-1.plcore > out.xml
-./plassert.py out.xml 'has_value(( Foo . r2 ), 17)'
-./run-pretty.sh app-2.plcore > out.xml
-./plassert.py out.xml 'has_value(( Foo . result ), 20)'
-
+./plassert.py app-1.plcore 'has_value(( Foo . r2 ), 17)'
+./plassert.py app-2.plcore 'has_value(( Foo . result ), 20)'
+./plassert.py app-3.plcore 'has_value(( Foo . r2 ), 5)'
+./plassert.py let-1.plcore 'has_value(( Foo . result ), 4)'
