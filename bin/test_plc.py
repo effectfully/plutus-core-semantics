58c7f1e46b5e8e95ab4a6209e16a1fecc3676c9a
Lucas Pena <lpena7@illinois.edu>
2018-04-05 16:25:35 -0500

    test: test case for recursive function (sumToN)

--- bin/test_plc.py
+++ bin/test_plc.py
@@ -77,8 +77,9 @@ def generate_tests(type):
             ("cmp-ops", "Foo", "equals",        [12, 17], False),
             ## ("cmp-ops", "Foo", "myTrue",        [],       True ),
 
-            ("case-simple", "SimpleCase", "simpleCase",  [13], 19),
-            ("case-simple", "SimpleCase", "simpleCase", [-13], 23),
+            ("case-simple", "SimpleCase", "simpleCase", [13],    19),
+            ("case-simple", "SimpleCase", "simpleCase", [-13],   23),
+            ("recursion",   "Recursion",  "sumToN",     [10, 0], 55),
            ]
     exec_no_error_code = [
             ("arith-ops", "Foo", "notPublic",   [19, 23],            ExitCode_NotPublic),
