b38af884e12ad1705af3341889ac4c57514ab530
Nishant Rodrigues <nishantjr@gmail.com>
2018-08-28 15:20:42 -0500

    HASH: Add overloads for Bytes -> Bytes

--- plugin/HASH.ml
+++ plugin/HASH.ml
@@ -1,27 +1,25 @@
 open Constants
 open Constants.K
 
-let do_hash str h =
-  let bytes = Cryptokit.hash_string h str in
-  let buf = Buffer.create ((String.length bytes) * 2) in
-  String.iter (fun c -> Buffer.add_string buf (Printf.sprintf "%02x" (int_of_char c))) bytes;
-  [String (Buffer.contents buf)]
+let do_hash_str: string -> Cryptokit.hash -> Constants.K.kitem list =
+  fun str h ->
+    let bytes = Cryptokit.hash_string h str in
+    let buf = Buffer.create ((String.length bytes) * 2) in
+    String.iter (fun c -> Buffer.add_string buf (Printf.sprintf "%02x" (int_of_char c))) bytes;
+    [String (Buffer.contents buf)]
 
-let hook_sha2_256 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.sha2 256 in
-  do_hash str h
-| _ -> failwith "sha2_256"
+let do_hash_bytes: bytes -> Cryptokit.hash -> Constants.K.kitem list =
+  fun bytes h ->
+    h#add_substring bytes 0 (Bytes.length bytes);
+    [Bytes (Bytes.of_string h#result)]
 
-let hook_keccak256 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.keccak 256 in
-  do_hash str h
-| _ -> failwith "keccak256"
+let do_hook: Cryptokit.hash -> string
+	      -> kitem list -> klabel -> sort -> kitem list -> (string -> k -> Z.t -> k) -> kitem list =
+  fun h failMsg c lbl sort config ff -> match c with
+    [String str]  ->  do_hash_str str h
+  | [Bytes bytes] ->  do_hash_bytes bytes h
+  | _ -> failwith failMsg
 
-let hook_ripemd160 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.ripemd160 () in
-  do_hash str h
-| _ -> failwith "ripemd160"
-
+let hook_sha2_256  = do_hook (Cryptokit.Hash.sha2 256)      "sha2_256"
+let hook_keccak256 = do_hook (Cryptokit.Hash.keccak 256)    "keccak256"
+let hook_ripemd160 = do_hook (Cryptokit.Hash.ripemd160 ())  "ripemd160"
